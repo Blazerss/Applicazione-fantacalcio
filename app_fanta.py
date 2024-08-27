2e53e5dd72a3fc001b93ee3077a57169b2c2ae8f
Blazerss <brontesii00@gmail.com>
2024-08-27 08:36:48 +0200

    Update app_fanta.py

--- app_fanta.py
+++ app_fanta.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import pandas as pd
+from streamlit_tags import st_tags
 import plotly.graph_objects as go
 import os
 
-st.title('Fanta 24-25')
+
+df = pd.read_excel("C:/Users/bront/OneDrive/Desktop/fanta.xlsx")
+st.title('Fanta')
+
+
+keywords = st_tags(label='# Inserisci giocatore:',text='Press enter to add more',value="",suggestions=df["Nome"].tolist())
+
+gioc = " ".join(keywords)
+df.set_index("Nome",inplace=True)
+print(gioc)
+if gioc:
+    st.write(f"Squadra: {df.loc[gioc.capitalize(),"Squadra"]}")
+    st.write(f"Attributi: {df.loc[gioc.capitalize(), "attributi"]}")
+    st.write(f"Resistenza Infortuni: {df.loc[gioc.capitalize(), "Res. Inf."]}")
+    st.write(f"Statistiche 21-22: {df.loc[gioc.capitalize(), ["Pv","Mv","Fm","Gf","Gs","Rp","Rc","R+","R-","Ass","Amm","Esp","Au"]]}")
